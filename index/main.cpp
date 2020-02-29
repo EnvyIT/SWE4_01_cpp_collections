853066357f1222836fa08f2babe32b6e0b55f9e5
Michael Eder <michael.eder@liwest.at>
2020-02-29 09:44:56 +0100

    * used set instead of vector * got rid of unique and sort

--- index/main.cpp
+++ index/main.cpp
@@ -1,5 +1,5 @@
 #include <fstream>
-#include <vector>
+#include <set>
 #include <string>
 #include <iterator>
 #include <algorithm>
@@ -13,23 +13,24 @@ using namespace std;
 int main(int argc , char * argv[]) {
   ifstream fin;
   open_stream(argc, argv, fin);
-  using cont_t = vector<string>;
   // vector<string> words;
+  using cont_t = set<string>; // O(n*log(n)) which has a red-black tree as underlying impl.
   cont_t words;
   transform(istream_iterator<string>(fin), //string s ; fin >> s;
             istream_iterator<string>(), //end of any istream
             //back_insert_iterator<vector<string>>(words), //inserting into container via
-            back_inserter<vector<string>>(words),
+            // back_inserter(words),
+           inserter(words, begin(words)),
             normalize);
   fin.close();
 
-  sort(begin(words),
+  /*sort(begin(words),
        end(words));
 
   cont_t::iterator new_end  = unique(begin(words),
          end(words));
   words.erase(new_end, 
-              end(words));
+              end(words));*/
 
   copy(begin(words),
        end(words),
